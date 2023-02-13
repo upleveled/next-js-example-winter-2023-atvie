7439845f97ed9e3f3ada02c5b42053aa43faf708
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-02-13 12:41:37 +0100

    Update eslint

--- app/animals/page.js
+++ app/animals/page.js
@@ -12,14 +12,6 @@ export const dynamic = 'force-dynamic';
 export default async function AnimalsPage() {
   const animals = await getAnimals();
 
-  // const animals = [
-  //   { id: 1, firstName: 'Dodo', type: 'turtle', accessory: 'scope' },
-  //   { id: 2, firstName: 'Paco', type: 'dog', accessory: 'jacket' },
-  //   { id: 3, firstName: 'Tira', type: 'cat', accessory: 'glasses' },
-  //   { id: 4, firstName: 'Danny', type: 'guineapig', accessory: 'zylinder' },
-  //   { id: 5, firstName: 'Karl', type: 'llama', accessory: 'hat' },
-  // ];
-
   return (
     <>
       <h1>Animals</h1>
