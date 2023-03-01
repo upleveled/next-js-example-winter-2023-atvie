81a65a278185e95a303852a0a1abb9ea1d0235ff
Karl Horky <karl.horky@gmail.com>
2023-03-01 15:31:19 +0100

    Remove unnecessary configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
     typedRoutes: true,
-    serverComponentsExternalPackages: ['bcrypt'],
   },
   eslint: {
     ignoreDuringBuilds: true,
