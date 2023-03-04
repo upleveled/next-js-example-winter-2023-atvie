222b7faa17773c0c797ad03cfcc78d779fad70c9
Karl Horky <karl.horky@gmail.com>
2023-03-04 18:32:15 +0100

    Add comment to regular expression

--- util/validation.ts
+++ util/validation.ts
@@ -1,7 +1,20 @@
 import { z } from 'zod';
 
 const returnToSchema = z.string().refine((value) => {
-  return !value.startsWith('/logout') && /^\/[\d#/=?a-z-]+$/.test(value);
+  return (
+    !value.startsWith('/logout') &&
+    // Regular expression for valid returnTo path:
+    // - starts with a slash
+    // - until the end of the string, 1 or more:
+    //   - numbers
+    //   - hash symbols
+    //   - forward slashes
+    //   - equals signs
+    //   - question marks
+    //   - lowercase letters
+    //   - dashes
+    /^\/[\d#/=?a-z-]+$/.test(value)
+  );
 });
 
 export function getSafeReturnToPath(path: string | string[] | undefined) {
