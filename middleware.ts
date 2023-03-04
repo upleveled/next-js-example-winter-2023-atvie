45252a1f1af72f4d5b74a487643fdc6bffc56277
Karl Horky <karl.horky@gmail.com>
2023-03-04 17:26:23 +0100

    Only apply Middleware on naive management routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const config = {
+  matcher: '/animal-management-naive-dont-copy/:path*',
+};
+
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   // Store current request pathname in a custom header
