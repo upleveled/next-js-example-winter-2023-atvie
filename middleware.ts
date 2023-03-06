b1a86e306e42d0d447487f3fd7e52f2745b342ba
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-03-06 17:48:10 +0100

    Update middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  if (
+    !request.nextUrl.pathname.startsWith('/animal-management-naive-dont-copy')
+  ) {
+    return NextResponse.next();
+  }
+
   const requestHeaders = new Headers(request.headers);
 
-  request.nextUrl.pathname.startsWith('/animal-management-naive-dont-copy') &&
-    requestHeaders.set('x-pathname', request.nextUrl.pathname);
+  requestHeaders.set('x-pathname', request.nextUrl.pathname);
 
   return NextResponse.next({
     request: {
