5fb2651e17d0c97f5654e781a943aa05e41a3c9a
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-03-06 17:31:39 +0100

    Break soft navigation from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-  matcher: '/animal-management-naive-dont-copy/:path*',
-};
-
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  // Store current request pathname in a custom header
-  requestHeaders.set('x-pathname', request.nextUrl.pathname);
+
+  request.nextUrl.pathname.startsWith('/animal-management-naive-dont-copy') &&
+    requestHeaders.set('x-pathname', request.nextUrl.pathname);
 
   return NextResponse.next({
     request: {
