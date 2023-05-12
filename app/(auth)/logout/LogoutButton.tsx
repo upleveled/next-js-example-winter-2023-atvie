9747bf60f11fa52150e1d9b5d1c84a905ab0c719
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-05-12 18:15:24 +0200

    Add comments to document Next.js actions

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -7,8 +7,11 @@ export default function LogoutButtonClient() {
     <form>
       <button
         className={styles.logoutButton}
+        // TODO: remove ts-ignore as soon Next.js implement the type update
         /* @ts-ignore */
         formAction={async () => {
+          // By adding use server we define this function as a server function that can be triggered from the client
+          // - https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions#server-functions
           'use server';
           const cookieStore = cookies();
           const token = cookieStore.get('sessionToken');
@@ -17,7 +20,11 @@ export default function LogoutButtonClient() {
             await deleteSessionByToken(token.value);
           }
 
-          (cookies() as any).set('sessionToken', '', {
+          // use the method to delete cookies only available in server actions and Route Handlers.
+          // - https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options
+          // TODO: remove ts-ignore as soon Next.js implement the type update
+          /* @ts-ignore */
+          await cookies().set('sessionToken', '', {
             maxAge: -1,
             expires: new Date(Date.now() - 10000),
           });
