357e43fb4f0cec6b44f5b9f4dde4373a0491fb14
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-05-17 16:57:41 +0200

    Remove extra clear button

--- app/fruits/[fruitName]/ActionFromClient.js
+++ app/fruits/[fruitName]/ActionFromClient.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { setFruitNote } from '../../../util/actions';
 
 export default function ActionFromClient(props) {
@@ -9,31 +9,17 @@ export default function ActionFromClient(props) {
   const router = useRouter();
 
   return (
-    <>
-      <form>
-        <input
-          readOnly
-          hidden={true}
-          value={props.fruitName}
-          name="fruit-name"
-        />
-        <textarea
-          name="fruit-note"
-          value={text}
-          onChange={(event) => {
-            setText(event.currentTarget.value);
-          }}
-          onBlur={() => router.refresh()}
-        />
-        <button formAction={setFruitNote}>Update Opinion</button>
-      </form>
-      <button
-        onClick={() => {
-          setText('');
+    <form>
+      <input readOnly hidden={true} value={props.fruitName} name="fruit-name" />
+      <textarea
+        name="fruit-note"
+        value={text}
+        onChange={(event) => {
+          setText(event.currentTarget.value);
         }}
-      >
-        Clear
-      </button>
-    </>
+        onBlur={() => router.refresh()}
+      />
+      <button formAction={setFruitNote}>Update Opinion</button>
+    </form>
   );
 }
