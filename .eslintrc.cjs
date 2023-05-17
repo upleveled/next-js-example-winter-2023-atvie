d6ae29d4d30b554362792418c6bc6c9bc8ab543a
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-05-17 11:36:29 +0200

    Install server-only package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,24 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
+  extends: ['@upleveled/upleveled'],
+  // plugins: ['@ts-safeql/eslint-plugin'],
+  // rules: {
+  //   '@ts-safeql/check-sql': [
+  //     'error',
+  //     {
+  //       connections: [
+  //         {
+  //           databaseUrl:
+  //             'postgres://graphql_database:graphql_database@localhost:5432/graphql_database',
+  //           tagName: 'sql',
+  //           fieldTransform: 'camel',
+  //           transform: '{type}[]',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = config;
