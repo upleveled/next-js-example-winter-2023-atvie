9d32ed876bd6c827fc8cd9715fc5ed4d5182f1ab
Lukas Prochazka <prochazka.lu@gmail.com>
2023-05-24 14:56:46 +0200

    Downgrade ESLint, revert localStorage.js

--- util/localStorage.js
+++ util/localStorage.js
@@ -1,12 +1,16 @@
+// more robust way to get items from localStorage
 export function getLocalStorage(key) {
-  // Is testing if we are in the browser
-  if (typeof window === 'undefined') return undefined;
-  return window.localStorage.getItem(key);
+  try {
+    return JSON.parse(window.localStorage.getItem(key));
+  } catch (err) {
+    return undefined;
+  }
 }
 
+// more robust way to set items to localStorage
 export function setLocalStorage(key, value) {
-  // Is testing if we are in the browser
+  // if we are in the browser
   if (typeof window !== 'undefined') {
-    window.localStorage.setItem(key, value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   }
 }
