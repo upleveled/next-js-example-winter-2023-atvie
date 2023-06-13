a1a06b6e23c1df60e3bb9a6ab4a0add8716678ac
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-06-13 14:40:34 +0200

    Add actions and remove test

---
+++ app/actions.ts
@@ -0,0 +1,18 @@
+'use server';
+import { cookies } from 'next/headers';
+import { deleteSessionByToken } from '../database/sessions';
+
+export async function logout() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('sessionToken');
+
+  if (token) {
+    await deleteSessionByToken(token.value);
+  }
+
+  // use the method to delete cookies only available in server actions and Route Handlers.
+  // - https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options
+  await cookies().set('sessionToken', '', {
+    maxAge: -1,
+  });
+}
